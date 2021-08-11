9025c0c6f02c2163e3a9b4e84e04bdcf2354f237
Majko <mrnhrnk@gmail.com>
2021-08-11 22:29:12 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import VisFileSelect from "./Components/nove/VisFileSelect";
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/upload">Upload image</Link>
+                </li>
+                <li>
+                  <Link to="/listdb">List DB Images</Link>
+                </li>
+                <li>
+                  <Link to="/multipage">Multi Page Image</Link>
+                </li>
+                <li>
+                  <Link to="/showallimgs">Show whole groups Images</Link>
+                </li>
+                <li>
+                  <Link to="/showmyimgs">Show my Images</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/showmyimgs">
+                <VisFileSelect />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
---
+++ src/Components/MultiPageImage.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { toPng } from "html-to-image";
+import ShowLocalImage from "./ShowLocalImage";
+
+/**
+ * @description Component that reds image files one by one, after which 
+ * it generates one image from the ones read
+ * @param {Object} props - No props needed 
+ * @returns no
+ */
+function MultiPageImage(props) {
+  const [multiFiles, setMultiFiles] = useState([]);
+  const [compositeImageUrl, setCompositeImageUrl] = useState(null);
+
+  /**
+   * @description Exports one image only as png for the selected HTML element
+   * containing more images, using html-to-image package
+   */
+  const exportAsPicture = async () => {
+    var data = document.getElementById("multipage");
+    try {
+      const dataUrl = await toPng(data);
+      setCompositeImageUrl(dataUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFileSelect = (file) => {
+    setMultiFiles((prevFiles) => [...prevFiles, file]);
+  };
+
+  /**
+   * @description Renders either selection of individual image files or One composite 
+   * image as composition of previous files
+   */
+  return (
+    <div className="App">
+      {!compositeImageUrl ? ( //check if export of big image exists, if not show file selector
+        <div id="select">
+          <h1>Pick few images and genererate onle file</h1>
+          <input
+            type="file"
+            onChange={(e) => handleFileSelect(e.target.files[0])}
+          />
+          <button onClick={exportAsPicture}>Generuj dokument</button>
+          <div id="multipage">
+            {multiFiles.map((file) => (
+              <ShowLocalImage file={file} key={file.name} />
+            ))}
+          </div>
+        </div>
+      ) : ( //if export exists, show one big image 
+        <div id="compositepage">
+          <img src={compositeImageUrl} alt="compostnyimg" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MultiPageImage;
